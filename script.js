c3ab9e8b232acd906c704d062eeda8cb5ed75f8f
griha1796 <69796095+griha1796@users.noreply.github.com>
2021-01-06 22:02:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+ $('#fullpage').fullpage({
+        navigation:true,
+        anchors:['firstPage','secondPage','3rdPage', '4rdPage', '5thPage', '6thPage', 'lastPage'],
+        menu: '#myMenu',
+         scrollOverflow: true ,
+        scrollingSpeed: 700,
+        fitToSectionDelay: 700,
+       
+
+ });
+
+});
+
+ 
